322c7b94191b8a36879bcbd3a496dd6ef786f001
pedroso-miguell <miguellarapedroso37@gmail.com>
2024-07-02 15:55:53 -0300

    Linktree semi-finalizada

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function toggleMode() {
-  const html = document.documentElement
-  html.classList.toggle('light')
+  function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
 }
